68dbc3efb79ff9bf3ab5a4959ad0e46c175f6394
jasonsait <162068163+jasonsait@users.noreply.github.com>
2024-03-03 18:30:29 -0700

    3

--- config/database.php
+++ config/database.php
@@ -140,10 +140,10 @@ return [
         'cache' => [
             'url' => env('REDIS_URL'),
             'scheme' => 'tls',
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
+            'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
+            'username' => env('AZURE_REDIS_USERNAME'),
+            'password' => env('AZURE_REDIS_PASSWORD'),
+            'port' => env('AZURE_REDIS_PORT', '6379'),
             'database' => env('AZURE_REDIS_DATABASE', '1'),
             
         ],
